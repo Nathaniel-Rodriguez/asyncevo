46bef37ba735f59cf9f2be9ca0e9352e3cb9d425
Nathaniel-Rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-02 17:46:15 -0600

    added member init func, and new pars for asyncga

--- asyncevo/scheduler.py
+++ asyncevo/scheduler.py
@@ -1,12 +1,35 @@
 __all__ = ['Scheduler']
 
 
+from typing import Dict
+
+
+def initialize_member(member_cls, member_parameters: Dict):
+    """
+
+    :param member_cls:
+    :param member_parameters:
+    :return:
+    """
+
+    return member_cls(**member_parameters)
+
+
 class Scheduler:
     """
 
     """
-    def __init__(self):
+    def __init__(self, num_workers: int,
+                 num_threads_per_worker: int = 1):
         """
 
         """
         pass
+
+    def initialize_population(self, member_cls, member_parameters):
+        """
+
+        :param member_cls:
+        :param member_parameters:
+        :return:
+        """
