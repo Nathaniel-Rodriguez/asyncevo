db245a15cb33e23ebf85d1d0953a08df3e8c7066
Nathaniel-Rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-02 01:50:09 -0600

    added RandNumTableMixin

--- asyncevo/member.py
+++ asyncevo/member.py
@@ -1,12 +1,26 @@
+from numpy import np
+
+
 class Member:
     """
 
     """
-
-    def __init__(self):
+    def __init__(self, member_size):
         """
 
         """
         pass
 
-    def
+
+class RandNumTableMixin:
+    """
+
+    """
+    def __init__(self, table_size, seed):
+        """
+
+        :param table_size:
+        :param seed:
+        """
+        self._rng = np.random.RandomState(seed)
+        self._table = self._rng.randn(table_size)
