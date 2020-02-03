c9fcc3062dad365ed6c22a4b69d3a0b957d605ad
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-03 00:49:38 -0600

    added comments for run

--- asyncevo/asyncga.py
+++ asyncevo/asyncga.py
@@ -21,20 +21,41 @@ class AsyncGa:
     """
 
     """
-    def __init__(self, population_size: int,
+    def __init__(self,
+                 population_size: int,
                  initializing_distribution: Distribution,
                  scheduler: Scheduler,
                  global_seed: int,
                  sigma: float,
                  crossover_probability: float = 1.0,
                  cooling_factor: float = 1.0,
-                 annealing_cut_off_step: int = -1):
+                 annealing_cut_off: int = -1):
+        self._population_size = population_size
+        self._initializing_distribution = initializing_distribution
+        self._scheduler = scheduler
+        self._global_seed = global_seed
+        self._sigma = sigma
+        self._crossover_probability = crossover_probability
+        self._cooling_factor = cooling_factor
+        self._annealing_cut_off = annealing_cut_off
         self._population_lineage = []
 
-    def run(self, num_iterations: int):
+    def run(self, num_iterations: int, lineages=None):
         """
 
         :param num_iterations:
+        :param lineages:
         :return:
         """
-        pass
+
+        # scatter member initializer
+        # generate lineages, if no lineages, else use available
+        # do initial map of fitness function with lineages
+        # define steps
+        # for loop over future as_completed
+            # get result
+            # ???
+            # check number of iterations, if not finished
+                # submit new work
+                # increment steps
+            # update lineages -> save to file
