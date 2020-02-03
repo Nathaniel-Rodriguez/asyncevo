ee9511e90b300e592312f903d938398a634d92b1
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-03 00:35:30 -0600

    updated dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+
 setup(name='asyncevo',
       version='0.0.1',
       description='Library of asyncronous distributed evolutionary algorithms',
@@ -8,7 +9,10 @@ setup(name='asyncevo',
       url='https://github.com/Nathaniel-Rodriguez/asyncevo.git',
       install_requires=[
           'dask>=2.10.1<3',  # maybe more than 3 and less than 2.10, probs 2
+          'dask-core>=2.10.1<3'
+          'distributed>=2.10.0',
           'numpy>=1.18.1',  # likely compatible with earlier versions.
+          'dask-mpi>=2.0.0'
           # sphinx?
       ],
       python_requires='>=3.7',
