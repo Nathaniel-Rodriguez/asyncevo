e3d0727a8e6b93b46c07fab75fd53beac4261094
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-04 22:59:27 -0600

    filled out member, lineage, an ga

--- asyncevo/lineage.py
+++ asyncevo/lineage.py
@@ -1,15 +1,25 @@
 __all__ = ['Lineage']
 
 
-from dataclasses import dataclass
-from typing import List
+from collections.abc import Sequence
 
 
-# lineage needs to be a tree of seeds for crossover
-# at each node on tree it needs a corresponding sigma
-@dataclass
-class Lineage:
+class Lineage(Sequence):
     """
-
+    A Lineage represents the compressed form of the parameters. The sequence of
+    seeds and sigmas are used to regenerate a member's parameters from some
+    initial vector.
     """
-    seeds: List[int]  # change to tree
+    def __init__(self, seed: int = None, sigma: float = None):
+        self._lineage = []
+        if not (seed is None) and not (sigma is None):
+            self._lineage.append({'seed': seed, 'sigma': sigma})
+
+    def add_history(self, seed: int, sigma: float):
+        self._lineage.append({'seed': seed, 'sigma': sigma})
+
+    def __getitem__(self, i):
+        return self._lineage[i]
+
+    def __len__(self):
+        return len(self._lineage)
--- asyncevo/member.py
+++ asyncevo/member.py
@@ -1,42 +1,72 @@
-__all__ = ['Member', 'RandNumTable']
+__all__ = ['Member']
 
 
 from numpy import np
-from asyncevo.distribution import Distribution
+from asyncevo.asyncga import AsyncGa
 from asyncevo.lineage import Lineage
+from asyncevo.sliceops import *
 
 
-# we don't need two members for crossover!
-# 1 member is enough.
-# we use the lineage -> integers RV for table
-# initializing dist is a problem
-# we have to go through add/subtract mutations for lineages on single member
-# lineage just says what mutation should be.. and they are all additive, so
-# order applied doesn't matter (and multiplicative, but same idea, it just
-# scales the addition)
 class Member:
     """
-
+    Represents an expression of a lineage from the population. Upon
+    initialization it generates the necessary allocations for appropriation
+    of a lineage. After this, lineage appropriation is relatively cheap and
+    the member can adopt new lineages, however it can only express a single
+    lineage at a given time.
     """
-    def __init__(self, initializing_distribution: Distribution):
+    def __init__(self, initial_state: np.ndarray, table_seed: int,
+                 table_size: int, max_table_step: int):
         """
-
+        :param initial_state: a numpy array from which to generate perturbations.
+        :param table_seed: the seed for generating the random number table.
+        :param table_size: the size of the random number table.
+        :param max_table_step: the maximum random stride for table slices
         """
-        self._initializing_distribution = initializing_distribution
-
-    def mutate(self, seed):
-        pass
-
-
-class RandNumTable:
-    """
-
-    """
-    def __init__(self, table_size: int, seed: int):
-        """
-
-        :param table_size:
-        :param seed:
-        """
-        self._rng = np.random.RandomState(seed)
+        self._x = initial_state
+        self._mutation = np.zeros(len(self._x), dtype=AsyncGa.dtype)
+        self._rng = np.random.RandomState(table_seed)
         self._table = self._rng.randn(table_size)
+        self._max_table_step = max_table_step
+        self._has_lineage = False
+        self._lineage = None
+
+    def appropriate_lineage(self, lineage: Lineage):
+        """
+        Take on a lineage and explicitly express its parameters.
+        :param lineage: a lineage to appropriate.
+        """
+        self._lineage = lineage
+        self._has_lineage = True
+        for mutation in self._lineage:
+            self._rng.seed(mutation['seed'])
+            param_slices = self._draw_random_parameter_slices(self._rng)
+            table_slices = self._draw_random_table_slices(self._rng)
+            param_slices, table_slices = match_slices(param_slices, table_slices)
+            # We assign the table values to the perturbation member first
+            multi_slice_assign(self._mutation, self._table,
+                               param_slices, table_slices)
+            # Apply the annealing scaling factor, i.e. the temperature
+            np.multiply(self._mutation, mutation['sigma'],
+                        out=self._mutation)
+            # With perturbation member complete, we can add to member
+            np.add(self._x, self._mutation, out=self._x)
+
+    def _draw_random_parameter_slices(self, rng):
+        """
+        Chooses a constrained slice subset of the parameters (start, stop, step)
+        to give roughly num_mutations perturbations (less if overlap if
+        step is too large)
+        """
+
+        return random_slices(rng, len(self._x), len(self._x), 1)
+
+    def _draw_random_table_slices(self, rng):
+        """
+        Chooses a constrained slice subset of the RN table (start, stop, step)
+        to give roughly num_mutations random numbers (less if overlap if
+        step is too large)
+        """
+
+        return random_slices(rng, len(self._table),
+                             len(self._x), self._max_table_step)
