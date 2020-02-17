a91d89a853c60cbecf07888a3b777f875b5b113c
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-16 22:30:18 -0600

    added argument for changing the calling pattern for fitness function

--- asyncevo/asyncga.py
+++ asyncevo/asyncga.py
@@ -10,6 +10,7 @@ from typing import Dict
 from typing import List
 from typing import Tuple
 from typing import Callable
+from typing import Union
 from asyncevo import Scheduler
 from asyncevo import Lineage
 from asyncevo import Member
@@ -29,31 +30,42 @@ def initialize_member(member_type, member_parameters: Dict) -> Member:
     return member_type(**member_parameters)
 
 
-def dispatch_work(fitness_function: Callable[[np.ndarray], float],
+def dispatch_work(fitness_function: Union[Callable[[Member], float],
+                                          Callable[[np.ndarray], float]],
                   lineage: Lineage,
                   member: Member,
                   member_id: int,
                   fitness_kwargs: Dict = None,
-                  is_initial: bool = False) -> Tuple[float, Lineage, int, bool]:
+                  is_initial: bool = False,
+                  take_member: bool = False) -> Tuple[float, Lineage, int, bool]:
     """
     Sends out work to a member and returns a tuple of the fitness and its
     associated lineage. The lineage is returned so that dispatch_work can
     be used in an asynchronous scenario where
     :param fitness_function: a callable object that takes member parameters
-    numpy array and returns a fitness value.
+    numpy array and returns a fitness value. It can also be a callable
+    that takes as an argument a Member or subclass of Member and returns the
+    fitness.
     :param lineage: the lineage for the member to use to generate parameters.
     :param member: an initialized member.
     :param member_id: id of the member.
     :param is_initial: specifies whether it is part of the initial dispatch
     :param fitness_kwargs: additional arguments for the fitness function
-    :return: fitness
+    :param take_member: whether the fitness function requires the member to be
+    provided or not (if not then expects an array) (default: False).
+    :return: fitness, lineage, member id, and is_initial
     """
     if fitness_kwargs is None:
         fitness_kwargs = {}
 
     member.appropriate_lineage(lineage)
-    return fitness_function(member.parameters, **fitness_kwargs),\
-           lineage, member_id, is_initial
+    if take_member:
+        return fitness_function(member, **fitness_kwargs),\
+               lineage, member_id, is_initial
+
+    else:
+        return fitness_function(member.parameters, **fitness_kwargs),\
+               lineage, member_id, is_initial
 
 
 class AsyncGa:
