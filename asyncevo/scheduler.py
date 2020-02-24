8c5d636bc11fc475464d3255e8f1258745d44787
Nathaniel-Rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-23 22:57:51 -0600

    added comments

--- asyncevo/scheduler.py
+++ asyncevo/scheduler.py
@@ -15,6 +15,75 @@ class Scheduler:
                  client_args: Dict = None):
         """
         Creating a scheduler initializes dask MPI
+        :param initialization_args: dictionary of keyword arguments for dask-mpi
+        initializer.
+        :param client_args: dictionary of keyword arguments for dask client.
+
+
+        dask-mpi arguments
+        ------------------
+        interface:str
+        Network interface like ‘eth0’ or ‘ib0’
+
+        nthreads:int
+        Number of threads per worker
+
+        local_directory:str
+        Directory to place worker files
+
+        memory_limit:int, float, or ‘auto’
+        Number of bytes before spilling data to disk. This can be an integer (nbytes), float (fraction of total memory), or ‘auto’.
+
+        nanny:bool
+        Start workers in nanny process for management
+
+        bokeh:bool
+        Enable Bokeh visual diagnostics
+
+        bokeh_port:int
+        Bokeh port for visual diagnostics
+
+        bokeh_prefix:str
+        Prefix for the bokeh app
+
+        bokeh_worker_port:int
+        Worker’s Bokeh port for visual diagnostics
+
+
+        dask client arguments
+        ---------------------
+        address: string, or Cluster
+        This can be the address of a Scheduler server like a string
+        '127.0.0.1:8786' or a cluster object like LocalCluster()
+
+        timeout: int
+        Timeout duration for initial connection to the scheduler
+
+        set_as_default: bool (True)
+        Claim this scheduler as the global dask scheduler
+
+        scheduler_file: string (optional)
+        Path to a file with scheduler information if available
+
+        security: Security or bool, optional
+        Optional security information. If creating a local cluster can also
+        pass in True, in which case temporary self-signed credentials will
+        be created automatically.
+
+        asynchronous: bool (False by default)
+        Set to True if using this client within async/await functions or within
+        Tornado gen.coroutines. Otherwise this should remain False for normal use.
+
+        name: string (optional)
+        Gives the client a name that will be included in logs generated on the
+        scheduler for matters relating to this client
+
+        direct_to_workers: bool (optional)
+        Whether or not to connect directly to the workers, or to ask the
+        scheduler to serve as intermediary.
+
+        heartbeat_interval: int
+        Time in milliseconds between heartbeats to scheduler
         """
         if initialization_args is None:
             initialization_args = {}
