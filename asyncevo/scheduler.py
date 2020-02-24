5d84fbc4a8188fed807676052d5d917804d87a52
Nathaniel-Rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-02-23 23:07:44 -0600

    changed variable names

--- asyncevo/scheduler.py
+++ asyncevo/scheduler.py
@@ -101,7 +101,7 @@ class Scheduler:
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         self._client.shutdown()
 
     @property
